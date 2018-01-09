4c44f400c096e8b5dbbdb74e3da3ffafb5ef0726
Roger Newman <roger.newman@halfaker.com>
2018-01-09 14:39:47 -0500

    Adding vba-corp-bgs maven project
    Need to configure binding and wsdl locations in maven

---
+++ mdm-cuf-person-adapter-vba-corp-bgs/src/main/java/org/mdm/cuf/person/adapter/vba/corp/bgs/App.java
@@ -0,0 +1,13 @@
+package org.mdm.cuf.person.adapter.vba.corp.bgs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
